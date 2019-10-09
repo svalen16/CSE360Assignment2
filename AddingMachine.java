90c6a27800297bcd493850132deb0ecc619e752b
svalen16 <56290957+svalen16@users.noreply.github.com>
2019-10-08 21:44:35 -0700

    Add files via upload

---
+++ AddingMachine.java
@@ -0,0 +1,57 @@
+package cse360assign2;
+
+/**
+ * 
+ * @author Szuching
+ * ASU ID: 1212544656
+ * Description: This program adds and subtracts values and prints the results.
+ */
+public class AddingMachine {
+
+	private int total;
+	
+	/***
+	 * This is a constructor that initializes the total to 0. 
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/***
+	 * Gets the total's value
+	 * @return this total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/***
+	 * Adds the value.
+	 * @param value This is the value being added.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/***
+	 * Subtracts the value.
+	 * @param value This is the value being subtracted.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/***
+	 * This returns a statement. 
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/***
+	 * This clears the value. 
+	 */
+	public void clear() {
+	
+	}
+}
